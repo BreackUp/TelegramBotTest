d64275925450fa7c75a938556718ca4974fa5fe7
BreackUp <108623515+BreackUp@users.noreply.github.com>
2022-07-20 20:30:27 +0300

    Ver 0.3
    Добавил гит хаб, сделал проверку с разным регистором

--- main.py
+++ main.py
@@ -11,18 +11,32 @@ def start(message):
 
 
 @bot.message_handler()
-def TEXTOTV(message):
+def main(message):
+    TestOTVcomm = False
     timenow = datetime.datetime.now()
-    if (message.text == "Да"):
-        mess = f'Вау, невероятно <b>{message.from_user.first_name}</b>, да ты крут!:з'
-        bot.send_message(message.chat.id, mess, parse_mode='html')
-    else:
+    if (message.text.upper().lower() == "да"):
+        Yes(message)
+        TestOTVcomm = True
+    if (message.text.upper().lower() == "привет"):
+        Hi(message)
+        TestOTVcomm = True
+    if(TestOTVcomm == False):
         bot.send_message(message.chat.id, "Прости, но я тебя не понимаю", parse_mode='html')
-    if(os.path.exists(path + '/' + str(message.chat.id) ) == False):
+    if (os.path.exists(path + '/' + str(message.chat.id)) == False):
         os.mkdir(f"{message.chat.id}")
+    print(message.text.upper().lower())
     my_file = open(f"{message.chat.id}\Text.txt", "a")
-    my_file.write(f"[{str(timenow)}]  " + message.from_user.username +": "+ message.text+"\n")
-    my_file.close( )
+    my_file.write(f"[{str(timenow)}]  " + message.from_user.username + ": " + message.text + "\n")
+    my_file.close()
 
+
+def Yes(name):
+    mess = f'Вау, невероятно <b>{name.from_user.first_name}</b>, да ты крут!:з'
+    bot.send_message(name.chat.id, mess, parse_mode='html')
+
+
+def Hi(name):
+    mess = f'И тебе привет <b>{name.from_user.username}</b>'
+    bot.send_message(name.chat.id, mess, parse_mode='html')
 
 bot.polling(none_stop=True)
